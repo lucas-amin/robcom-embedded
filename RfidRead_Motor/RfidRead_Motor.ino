0fde999a2fe6a85359bf07bb6d8f75e7eccce11e
lucas-amin <lucas_amin_2@Hotmail.com>
2018-11-28 20:29:08 -0200

    Change reverse speed and iterations

--- RfidRead_Motor/RfidRead_Motor.ino
+++ RfidRead_Motor/RfidRead_Motor.ino
@@ -41,69 +41,56 @@
 #define KP 0
 #define KD 0
 #define LINE 2000
-#define BASESPD 150
-#define MAXSPD 220
+#define BASESPD 170
+#define MAXSPD 230
 
 constexpr uint8_t RST_PIN = 2;          // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = 53;         // Configurable, see typical pin layout above
 constexpr uint8_t BUZZER = 12;         // Configurable, see typical pin layout above
 
-uint8_t lastState = 0;         // Configurable, see typical pin layout above
-uint8_t state = 0;         // Configurable, see typical pin layout above
+int state = 0;         // Configurable, see typical pin layout above
 int bluetooth_counter = 0;         // Configurable, see typical pin layout above
 
+constexpr uint8_t DISPENSER_STATE = 1;               // Configurable, see typical pin layout above
+constexpr uint8_t LIQUID_DROP_STATE = 3;             // Configurable, see typical pin layout above
+constexpr uint8_t FIRST_CURVE_RFID_STATE = 5;        // Configurable, see typical pin layout above
+constexpr uint8_t SECOND_CURVE_RFID_STATE = 7;       // Configurable, see typical pin layout above
+constexpr uint8_t THIRD_CURVE_RFID_STATE = 9;        // Configurable, see typical pin layout above
 
-constexpr uint8_t DISPENSER_STATE = 1;         // Configurable, see typical pin layout above
-constexpr uint8_t LIQUID_DROP_STATE = 3;         // Configurable, see typical pin layout above
-constexpr uint8_t FIRST_CURVE_RFID_STATE = 5;         // Configurable, see typical pin layout above
-constexpr uint8_t SECOND_CURVE_RFID_STATE = 7;         // Configurable, see typical pin layout above
-constexpr uint8_t THIRD_CURVE_RFID_STATE = 9;         // Configurable, see typical pin layout above
+constexpr uint8_t START_STATE = 0;                   // Configurable, see typical pin layout above
+constexpr uint8_t CUP_DEPLOYED_STATE = 2;            // Configurable, see typical pin layout above
 
-
-constexpr uint8_t START_STATE = 0;         // Configurable, see typical pin layout above
-
-constexpr uint8_t CUP_DEPLOYED_STATE = 2;         // Configurable, see typical pin layout above
-
-constexpr uint8_t TABLE_ONE_STATE = 4;         // Configurable, see typical pin layout above
-constexpr uint8_t TABLE_TWO_STATE = 6;         // Configurable, see typical pin layout above
-constexpr uint8_t TABLE_THREE_STATE = 8;         // Configurable, see typical pin layout above
+constexpr uint8_t TABLE_ONE_STATE = 4;               // Configurable, see typical pin layout above
+constexpr uint8_t TABLE_TWO_STATE = 6;               // Configurable, see typical pin layout above
+constexpr uint8_t TABLE_THREE_STATE = 8;             // Configurable, see typical pin layout above
 
 constexpr uint8_t WAITING_CLIENT_STATE = 10;         // Configurable, see typical pin layout above
-
-constexpr uint8_t GOING_BACK_STATE = 12; // Configurable, see typical pin layout above
-
+constexpr uint8_t GOING_BACK_STATE = 12;             // Configurable, see typical pin layout above
 
 //------------------------------------------------------------------IDS Cadastrados-------------------------------------------------------------------//
-
-
 MFRC522::Uid dispenserRFID = {4, {0x29, 0x4B, 0x0B, 0x0E}, 0xff}; // cartao1
 MFRC522::Uid dropLiquidRFID = {4, {0x49, 0x8F, 0x39, 0x06}, 0xff}; // cartao2
+
 MFRC522::Uid firstCurveRFID = {4, {0xB9, 0x64, 0x90, 0x01}, 0xff}; // cartao3
-MFRC522::Uid secondCurveRFID = {4, {0x59, 0x0F, 0xCB, 0x01}, 0xff}; // cartao4
-MFRC522::Uid thirdCurveRFID = {4, {0x11, 0xC3, 0x40, 0x00}, 0xff}; // cartao5
-
 MFRC522::Uid firstStopRFID = {4, {0x70, 0xD7, 0x5F, 0xA8}, 0xff}; // cartao6
-MFRC522::Uid secondStopRFID = {4, {0xD1, 0xFD, 0x7D, 0x6D}, 0xff}; // cartao3
-MFRC522::Uid thirdStopRFID = {4, {0x30, 0xA2, 0x57, 0xA8}, 0xff}; // cartao3
 
+MFRC522::Uid secondCurveRFID = {4, {0x59, 0x0F, 0xCB, 0x01}, 0xff}; // cartao4
+MFRC522::Uid secondStopRFID = {4, {0xD1, 0xFD, 0x7D, 0x6D}, 0xff}; // cartao7
 
-MFRC522::Uid tagRFID = {4, {0x04, 0x1C, 0x98, 0xEB}, 0xff}; // tag
-//tagRFID.size = 4
-//tagRFID.uidByte = {0x04, 0x1C, 0x98, 0xEB} ; // uid do card2
-//(size,uid,whatevs)
+MFRC522::Uid thirdCurveRFID = {4, {0x11, 0xC3, 0x40, 0x00}, 0xff}; // cartao5
+MFRC522::Uid thirdStopRFID = {4, {0x30, 0xA2, 0x57, 0xA8}, 0xff}; // cartao8
+
+// MFRC522::Uid tagRFID = {4, {0x04, 0x1C, 0x98, 0xEB}, 0xff}; // tag
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-uint8_t destiny = -1;
 bool reverse = false;
 bool curveCompleted = false;
 
+// ---------------------------------------------------------  Bluetooth variables  ----------------------------------------
+char data = 0; //Variable for storing received data
 
-// ------------------------------- Bluetooth variables ----------------------------------------
-char data = 0;            //Variable for storing received data
-
-
-//---------------------------------------------- Check cups  ----------------------------------------------//
+//----------------------------------------------------------  Check cups  ----------------------------------------------//
 bool haveCup = true;
 int LDRValue = 0;     // result of reading the analog pin
 
@@ -111,39 +98,38 @@ void updateCupState(){
   LDRValue = analogRead(LDRpin); // read the value from the LDR
   
   Serial.println(LDRValue);
-  if (LDRValue > 500) {
+  if (LDRValue > 900) {
     haveCup = false;
     // Serial.println("Dont have cup");      // print the value to the serial port
   } else {
     haveCup = true;
     // Serial.println("Has cup");      // print the value to the serial port
-  }  
+  }
 }
 
 void buzz() {
-  tone(BUZZER,500);
+  tone(BUZZER,500);  
   delay(400);
   noTone(BUZZER);
   delay(400);
 }
-  
-//------------------------------------------------------------------Controle do movimento -------------------------------------------------------------------//
 
-float IMPULSE_CONSTANT = 1.3;
+void buzz_start() {
+  tone(BUZZER,500);  
+}
 
+void buzz_stop() {
+   noTone(BUZZER);
+}
+
+//-------------------------------------------------------  Controle do movimento  -------------------------------------------------------------------
 /* MOTOR 1 -->
   MOTOR 2  <-- 
   MOTOR 3  V
-            */
-int direction_number = 0;
-int FRONT = 1;
-int LEFT = 2;
-int RIGHT = 3;
-int counter = 0;
-
+*/
 int reverseCounter = 0;
-
 int side = 0;
+
 float pos = LINE, error = 0, lError = 0, lPos;
 
 constexpr uint8_t MOTOR3_SPD = 4;         // Configurable, see typical pin layout above
@@ -167,9 +153,9 @@ constexpr uint8_t S5 = 46;
 
 //------------------------------------------------------ Turn left  -----------------------------------------------------
 void turnLeft() {
-  analogWrite(MOTOR1_SPD,150);
-  analogWrite(MOTOR2_SPD,150);
-  analogWrite(MOTOR3_SPD,150);
+  analogWrite(MOTOR1_SPD, BASESPD);
+  analogWrite(MOTOR2_SPD, BASESPD);
+  analogWrite(MOTOR3_SPD, BASESPD);
   
   digitalWrite(MOTOR1_IN1, HIGH);
   digitalWrite(MOTOR1_IN2, LOW);
@@ -180,15 +166,15 @@ void turnLeft() {
   digitalWrite(MOTOR3_IN1, LOW);
   digitalWrite(MOTOR3_IN2, HIGH);
   Serial.print("Virando 90 graus para esquerda\n");
-  delay(500);
+  delay(400);
 }
 
 
 //------------------------------------------------------ Turn right -----------------------------------------------------
 void turnRight() {
-  analogWrite(MOTOR1_SPD,150);
-  analogWrite(MOTOR2_SPD,150);
-  analogWrite(MOTOR3_SPD,150);
+  analogWrite(MOTOR1_SPD,BASESPD);
+  analogWrite(MOTOR2_SPD,BASESPD);
+  analogWrite(MOTOR3_SPD,BASESPD);
   
   digitalWrite(MOTOR1_IN1, LOW);
   digitalWrite(MOTOR1_IN2, HIGH);
@@ -199,29 +185,27 @@ void turnRight() {
   digitalWrite(MOTOR3_IN1, HIGH);
   digitalWrite(MOTOR3_IN2, LOW);
   Serial.print("Virando 90 graus para direita\n");
-  delay(500);
+  delay(400);
 }
 
-
-//------------------------------------------------------ Forward -----------------------------------------------------
+// -----------------------------------------------  Forward  ---------------------------------------------------
 void stop() {
   digitalWrite(MOTOR1_IN1, HIGH);
   digitalWrite(MOTOR1_IN2, HIGH);
 
-  // Mantain current flow to avoid power bank disable
+  // Keep current flow to avoid power bank disable
   digitalWrite(MOTOR2_IN1, HIGH);
-  digitalWrite(MOTOR2_IN2, LOW);
-  analogWrite(MOTOR2_SPD, 50);
+  digitalWrite(MOTOR2_IN2, HIGH);
       
-  digitalWrite(MOTOR3_IN1, HIGH);
+  digitalWrite(MOTOR3_IN1, LOW);
   digitalWrite(MOTOR3_IN2, HIGH);
+  analogWrite(MOTOR3_SPD, 5);
 }
 
-  
 void turnBackwards() {
-  analogWrite(MOTOR1_SPD,BASESPD);
-  analogWrite(MOTOR2_SPD,BASESPD);
-  analogWrite(MOTOR3_SPD,BASESPD);
+  analogWrite(MOTOR1_SPD, BASESPD);
+  analogWrite(MOTOR2_SPD, BASESPD);
+  analogWrite(MOTOR3_SPD, BASESPD);
   
   digitalWrite(MOTOR1_IN1, HIGH);
   digitalWrite(MOTOR1_IN2, LOW);
@@ -229,13 +213,10 @@ void turnBackwards() {
   digitalWrite(MOTOR2_IN1, LOW);
   digitalWrite(MOTOR2_IN2, HIGH);
   
-  digitalWrite(MOTOR3_IN1, HIGH);
-  digitalWrite(MOTOR3_IN2, LOW);
-  Serial.print("Virando 180 graus\n");
-  delay(1000);
-  
-  reverse = false;
-  reverseCounter = 0;
+  digitalWrite(MOTOR3_IN1, LOW);
+  digitalWrite(MOTOR3_IN2, HIGH);
+  Serial.print("Virando 180 graus para esquerda\n");
+  delay(500);
 }
 
 
@@ -254,20 +235,21 @@ void control()
   int ps3 = !sensor3*2000;
   int ps4 = !sensor4*3000;
   int ps5 = !sensor5*4000;
-
+  
   // Check if robot is waiting bluetooth response on top of dispenser or liquid drop RFID
   if (state  == DISPENSER_STATE or state == LIQUID_DROP_STATE) {
     stop();
     
   // Check if robot is waiting for client to pickup cup and activates buzzer if positive.
   } else if (state == WAITING_CLIENT_STATE) {
-    stop();
-      
     updateCupState();
 
     // Check cup state, make noise if the client didn`t catch the cup
     if (haveCup) {
+      stop();
       buzz();
+
+    // Check cup state, go back to dispenser if cup is not present anymore
     } else {
       state = GOING_BACK_STATE;
       turnBackwards();
@@ -277,7 +259,7 @@ void control()
   // Check all sensors and apply PD algorithm to follow line
   else if(!sensor1 or !sensor2 or !sensor3 or !sensor4 or !sensor5) {
     lPos = pos;
-    pos = (ps1 + ps2+ ps3 + ps4 + ps5)/(!sensor1 + !sensor2 + !sensor3 + !sensor4 + !sensor5);
+    pos = (ps1 + ps2+ ps3 + ps4 + ps5) / (!sensor1 + !sensor2 + !sensor3 + !sensor4 + !sensor5);
     lError = error;
     error = pos - LINE;
     float k = 0.015;
@@ -285,26 +267,31 @@ void control()
     int spd1 = (BASESPD - motorSpd) * 1.25;
     int spd2 = BASESPD + motorSpd;
     
-    if(spd1>MAXSPD) {
+    if(spd1 > MAXSPD) {
       spd1 = MAXSPD;
     }
-    if(spd2>MAXSPD) {
+    if(spd2 > MAXSPD) {
       spd2 = MAXSPD;
     }
     
     // Move backwards to leave dispenser
     if (reverse) {
-      // Reverse during 50 iterations
-      if (reverseCounter < 75) {
+      Serial.println("Reverse");
+      
+      buzz_start();
+      
+      // Reverse during 65 iterations
+      if (reverseCounter < 65) {
         Serial.println("Reverse");
-        analogWrite(MOTOR1_SPD, spd2);
-        analogWrite(MOTOR2_SPD, spd1);
+        analogWrite(MOTOR1_SPD, spd2 * 1.1);
+        analogWrite(MOTOR2_SPD, spd1 * 1.1);
         
         digitalWrite(MOTOR1_IN1, LOW);
         digitalWrite(MOTOR1_IN2, HIGH);
       
         digitalWrite(MOTOR2_IN1, LOW);
         digitalWrite(MOTOR2_IN2, HIGH);
+
         reverseCounter++;
 
       // Turn 180 degrees after leaving dispenser to continue its delivery
@@ -314,7 +301,10 @@ void control()
         reverseCounter = 0;
         turnBackwards();
       }
+
     } else {
+      buzz_stop();
+      
       analogWrite(MOTOR1_SPD, spd1);
       analogWrite(MOTOR2_SPD, spd2);
           
@@ -330,7 +320,6 @@ void control()
     digitalWrite(MOTOR3_IN1, HIGH);
     digitalWrite(MOTOR3_IN2, HIGH);
   }
-  
 }
 
 // ------------------------------------- Write and read from bluetooth -----------------------------------
@@ -338,11 +327,11 @@ void control()
 // Read from bluetooth when information is available
 char readBluetooth()
 {
-  data = ',';
+   data = ',';
    if(Serial3.available()> 0) { // Send data only when you receive data:
       data = Serial3.read();        //Read the incoming data & store into data
       Serial.print(data);          //Print Value inside data in Serial monitor
-      Serial.print("   ok\n");
+      Serial.print("   received\n");
    }
    return data;
 }
@@ -360,15 +349,13 @@ void writeBluetooth(char writeData) {
 // Changes of state related to RFID stops
 void checkRFID()
 {
-  Serial.println(mfrc522.uid.uidByte[0]);
-  
-  if (mfrc522.uid.uidByte[0] == dispenserRFID.uidByte[0] && state == START_STATE) {
+  if (mfrc522.uid.uidByte[0] == dispenserRFID.uidByte[0] && (state == START_STATE || state == GOING_BACK_STATE)) {
     state = DISPENSER_STATE;
   }
   
   if (mfrc522.uid.uidByte[0] == dropLiquidRFID.uidByte[0] && state == CUP_DEPLOYED_STATE) {
     state = LIQUID_DROP_STATE;
-    curveCompleted = false; 
+    curveCompleted = false; ;
   }
   
   
@@ -379,7 +366,7 @@ void checkRFID()
     } else if (!curveCompleted) {
       turnLeft();
       curveCompleted = true;
-    }
+    } 
   }
   
   if (mfrc522.uid.uidByte[0] == secondCurveRFID.uidByte[0] && (state == TABLE_TWO_STATE || state == GOING_BACK_STATE)) {
@@ -413,49 +400,40 @@ void checkRFID()
   if (mfrc522.uid.uidByte[0] == thirdStopRFID.uidByte[0] && (state == TABLE_THREE_STATE)) {
       state = WAITING_CLIENT_STATE;
   }
-  
-  lastState = state;
 }
 
 // ------------------------------------------ Bluetooth change of states --------------------------------
 // Changes of state related to bluetooth
 void changeState(){
   // Check if data received is other than default
-  if (data != ',')  {
+  if (data != ',' && data != 'a')  {
   
     // Check if data received as char is equal to the state converted to char
-    // obs: Char '1' == Hex 0x31
-    
     // Change to cup deployed state when base station sends character '2'
-    if ((int) data == (CUP_DEPLOYED_STATE + 0x30)) {
+    updateCupState();
+    if ((data == (char) (CUP_DEPLOYED_STATE + 0x30)) && haveCup) {
       Serial.println("Changed to state 2");
-      lastState = CUP_DEPLOYED_STATE;
       state = CUP_DEPLOYED_STATE;
 
     // Change to table one state when base station sends character '4'
-    } else if ((int) data == (TABLE_ONE_STATE + 0x30)) {
+    Serial.println(data == (char) (TABLE_ONE_STATE + 0x30));
+    } else if (data == (char) (TABLE_ONE_STATE + 0x30) && state == LIQUID_DROP_STATE) {
       Serial.println("Changed to state 4");
-      lastState = TABLE_ONE_STATE;
       state = TABLE_ONE_STATE;
-      destiny = FIRST_CURVE_RFID_STATE;
-      
-      reverse = true;
-      
-    // Change to table two state when base station sends character '6'
-    }  else if ((int) data == (TABLE_TWO_STATE + 0x30)) {
-      Serial.println("Changed to state 6");
-      lastState = TABLE_TWO_STATE;
-      state = TABLE_TWO_STATE;
-      destiny = SECOND_CURVE_RFID_STATE;
 
       reverse = true;
       
+    // Change to table two state when base station sends character '6'
+    }  else if (data == (char) (TABLE_TWO_STATE + 0x30) && state == LIQUID_DROP_STATE) {
+      Serial.println("Changed to state 6");
+      state = TABLE_TWO_STATE;
+      
+      reverse = true;
+      
     // Change to table three state when base station sends character '8'
-    }  else if ((int) data == (TABLE_THREE_STATE + 0x30)) {
+    }  else if (data == (int) (TABLE_THREE_STATE + 0x30) and state == LIQUID_DROP_STATE) {
       Serial.println("Changed to state 8");
-      lastState = TABLE_THREE_STATE;
       state = TABLE_THREE_STATE;
-      destiny = THIRD_CURVE_RFID_STATE;
 
       reverse = true;
     }
@@ -463,8 +441,7 @@ void changeState(){
 }
 
 // ------------------------------- Send state through bluetooth --------------------------------------------
-
-void updateStateBluetooth() {
+void sendStateBluetooth() {
   // Only sends information every 50th loop iteration
   if ((bluetooth_counter % 50) == 0) {
   
@@ -472,7 +449,7 @@ void updateStateBluetooth() {
     if ((state == 1 || state == 3)) {
       Serial.println("Data sent to bluetooth");
       Serial.println((char) (state + 0x30));
-      writeBluetooth((lastState + 0x30));
+      writeBluetooth((char) (state + 0x30));
 
       // Bluetooth delay time for synchronization with base station
       delay(1500);
@@ -483,7 +460,6 @@ void updateStateBluetooth() {
 }
 
 // -------------------------------------------------- Setup ----------------------------------------------------
-
 void setup() {
   // Inicializar bluetoothj
   //Initialize Bluetooth Serial Port
@@ -495,7 +471,6 @@ void setup() {
   pinMode(S3, INPUT);
   pinMode(S4, INPUT);
   pinMode(S5, INPUT);
-  direction_number = 0;
 
   //Pinos dos motores
   pinMode(MOTOR1_IN1, OUTPUT);
@@ -523,18 +498,17 @@ void setup() {
 
 // ------------------------------------------------- MAIN LOOP ---------------------------------------------
 void loop() {
-  // Check the sensors to control the motors
-  
   // RFID state changes
   checkRFID();
-  
-  // Bluetooth state changes
-  updateStateBluetooth();
+
+  // Update state based on RFID and Bluetooth updates
+  changeState();
 
   // Control routine
   control();
   
-  changeState();
+  // Bluetooth state changes
+  sendStateBluetooth();
   
   // Write data from HC06 to Serial Monitor
   data = readBluetooth();
@@ -547,5 +521,4 @@ void loop() {
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return; 
   }
-
 }
